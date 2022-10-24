731c294fff2ff702eed6bc1076a6469b92c59081
LouisRibardiereENI <103489475+louisribardiereENI@users.noreply.github.com>
2022-10-24 23:03:51 +0200

    Participant liste + details

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ParticipantRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/participants', name: 'app_participant')]
+
+class ParticipantController extends AbstractController
+{
+    #[Route('/', name: 'list')]
+    public function index(ParticipantRepository $repo): Response
+    {
+        $participants = $repo->findAll();
+        return $this->render('participant/index.html.twig', [
+            'controller_name' => 'ParticipantController',
+            'participants' =>$participants,
+        ]);
+    }
+    #[Route('/{id}', name: 'details')]
+    public function detail(ParticipantRepository $repo,int $id): Response
+    {
+        $participant = $repo->find($id);
+        return $this->render('participant/detail.html.twig', [
+            'controller_name' => 'ParticipantController',
+            'participant' =>$participant,
+        ]);
+    }
+}
