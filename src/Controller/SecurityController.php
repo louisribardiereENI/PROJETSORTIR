12c4d238142e8d8f59f74f2b77b334d3cea952bb
baptistecoutanteni <baptiste.coutant2021@campus-eni.fr>
2022-10-25 18:31:58 +0200

    connexion + modif routage

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,18 +9,14 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/connexion', name: 'app_login')]
+    #[Route(path: '/', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
+        if ($this->getUser()) {
+             return $this->redirectToRoute('app_home');
+        }
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
