efafdaba5cc01c557c0976492e23c33febd440ae
baptistecoutanteni <baptiste.coutant2021@campus-eni.fr>
2022-10-26 09:04:36 +0200

    mailling

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
 #[Route('/', name: 'app_')]
 class HomeController extends AbstractController
@@ -15,4 +20,25 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    #[Route('/email', name: 'email')]
+    public function emailtest(MailerInterface $mailer,): Response
+    {
+        $email = (new TemplatedEmail())
+            ->from(new Address('smtp@baptiste-coutant.fr', 'Sortir'))
+            ->to('baptiste.coutant2021@campus-eni.fr')
+            ->subject('Your password reset request')
+            ->htmlTemplate('reset_password/test.html.twig');
+
+        $mailer->send($email);
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
 }
+
