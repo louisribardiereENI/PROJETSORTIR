3aa87e3a7dbce15a76592cb3fcb4408e0cf819f4
baptistecoutanteni <baptiste.coutant2021@campus-eni.fr>
2022-10-27 09:50:03 +0200

    Research bar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,6 +16,8 @@ class HomeController extends AbstractController
     #[Route('/accueil', name: 'home')]
     public function index(): Response
     {
+
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
 
